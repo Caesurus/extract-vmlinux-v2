e14d3f08afb8ab081b069476558f4cfea0c57fc3
Tim dW <tim@finitestate.io>
2020-10-19 12:55:37 -0400

    Adding version info

--- extract-vmlinux-v2.go
+++ extract-vmlinux-v2.go
@@ -14,12 +14,10 @@ import (
 )
 
 func main() {
-	parser := argparse.NewParser("extract-vmlinux-v2", "A more robust vmlinux extractor")
-	opts := argparse.Options{}
-	opts.Required = true
-
-	var kernelFile *os.File = parser.File("f", "file", os.O_RDWR, os.FileMode(0600), &opts)
-	var ignoreValidation *bool = parser.Flag("i", "ignore", &argparse.Options{})
+	parser := argparse.NewParser(appName, "A more robust vmlinux extractor")
+	var inputFile *string = parser.String("f", "file", &argparse.Options{Help: "Input file to be processed"})
+	var ignoreValidation *bool = parser.Flag("i", "ignore", &argparse.Options{Help: "Ignore kernel verification, extract whatever is found"})
+	var printVersion *bool = parser.Flag("V", "version", &argparse.Options{Help: "Print version info"})
 
 	// Parse input
 	err := parser.Parse(os.Args)
@@ -28,23 +26,36 @@ func main() {
 		os.Exit(1)
 	}
 
-	data, err := ioutil.ReadAll(kernelFile)
-	if err != nil {
-		log.Fatal(err)
+	if *printVersion {
+		fmt.Printf("%s: %d.%d.%d\n", appName, versionMajor, versionMinor, versionPatch)
+		os.Exit(0)
 	}
-	ke := NewKernelExtractor(&data, *ignoreValidation)
-	files := ke.ExtractAll()
 
-	for filename, extractedData := range files {
-		file, err := ioutil.TempFile("", filename)
+	if len(*inputFile) > 0 {
+		kernelFile, err := os.Open(*inputFile)
+		if err != nil {
+			fmt.Println("couldn't open file")
+			os.Exit(2)
+		}
+
+		data, err := ioutil.ReadAll(kernelFile)
 		if err != nil {
 			log.Fatal(err)
 		}
-		bytesWritten, err := file.Write(extractedData)
-		if err == nil {
-			fmt.Printf("Wrote %d bytes to file: %s\n", bytesWritten, file.Name())
-		} else {
-			fmt.Println("Extraction failed", err)
+		ke := NewKernelExtractor(&data, *ignoreValidation)
+		files := ke.ExtractAll()
+
+		for filename, extractedData := range files {
+			file, err := ioutil.TempFile("", filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bytesWritten, err := file.Write(extractedData)
+			if err == nil {
+				fmt.Printf("Wrote %d bytes to file: %s\n", bytesWritten, file.Name())
+			} else {
+				fmt.Println("Extraction failed", err)
+			}
 		}
 	}
 
---
+++ version.go
@@ -0,0 +1,8 @@
+package main
+
+var (
+	appName = "extract-vmlinux-v2"
+	versionMajor = 0
+	versionMinor = 1
+	versionPatch = 0
+)
